14e816d7ee839847c29ef9f42ef44e45db715a53
nachorca <jsantiagomartin@icloud.com>
2026-05-14 18:48:51 +0200

    Fix dynamic pricing deployment

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -271,7 +271,7 @@ export default function AdminPage() {
           </div>
 
           {showCalendar && (
-            <AdminCalendar reservations={unifiedRows} />
+            <AdminCalendar reservations={unifiedRows} adminPassword={password} />
           )}
         </div>
 
